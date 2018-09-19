e6d2018f17b1679ee2558ce92a07131d91a63578
LGMchili <520100297@qq.com>
2018-09-19 22:41:02 +0800

    add comment

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,7 +14,12 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
+    // take variable parameters
     template<class F, class... Args>
+    // rvalue reference
+    // new syntax for function declaration in c++11
+    // old : return-type identifier ( argument-declarations... )
+    // new : auto identifier ( argument-declarations... ) -> return_type
     auto enqueue(F&& f, Args&&... args)
         -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
@@ -38,8 +43,10 @@ inline ThreadPool::ThreadPool(size_t threads)
         // construct threads using below function
         workers.emplace_back(
             // a void function
+            // these threads will wait here at the beginning
             [this]
             {
+                // keep executing while there are still unfinished tasks
                 for(;;)
                 {
                     std::function<void()> task;
@@ -53,10 +60,11 @@ inline ThreadPool::ThreadPool(size_t threads)
                         // return when receive a stop signal or the tasks are empty
                         if(this->stop && this->tasks.empty())
                             return;
+                        // transfer it to task, using rvalue reference
                         task = std::move(this->tasks.front());
                         this->tasks.pop();
                     }
-
+                    // execute the task
                     task();
                 }
             }
@@ -68,12 +76,17 @@ template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
+    // get result type
     using return_type = typename std::result_of<F(Args...)>::type;
-
+    // make_shared wraps it to a shared_ptr
+    // A packaged_task wraps a callable element and allows its result to be retrieved asynchronously.
     auto task = std::make_shared< std::packaged_task<return_type()> >(
+            // bind return a function object, with its arguments bound to args.
+            // forward returns an rvalue reference to arg if arg is not an lvalue reference.
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-
+    // packaged_task -> get_future
+    // a future object associated with the object's shared state.
     std::future<return_type> res = task->get_future();
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
@@ -84,6 +97,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 
         tasks.emplace([task](){ (*task)(); });
     }
+    // Unblocks one of the threads (arbitrarily) currently waiting for this condition.
     condition.notify_one();
     return res;
 }
